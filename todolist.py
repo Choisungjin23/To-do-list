a41316648caf09c3c12836ec57db5d5f9eff7c8e
choi sungjin <sung004jin@gmail.com>
2025-11-25 00:40:34 +0900

    day 1

---
+++ todolist.py
@@ -0,0 +1,17 @@
+# python learning day 1
+print("python learning todolist.py")
+
+name = input("Enter your name:")
+task1 = input("Enter that you want to do:")
+task2 = input("Enter that you want to do next:")
+tastk3 = input("Enter that you want to do last:")
+
+print("------------------todo list------------------")
+print("Name:", name)
+print("Task:",task)
+print(name+"have a good day finishing up "+ task1+","+task2+" and  "+task3)
+print("---------------------------------------------")
+
+print("what is the most important task?")
+priority = input("tell me:")
+print("Do it fast as you can !"+priority)
