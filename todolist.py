5c65433a9cadf8beee64ea0ff57a8cafdb3fdb13
choi sungjin <sung004jin@gmail.com>
2025-11-26 02:03:00 +0900

    Day 2 commit

--- todolist.py
+++ todolist.py
@@ -1,4 +1,5 @@
 # python learning day 1
+"""
 print("python learning todolist.py")
 
 name = input("Enter your name:")
@@ -15,3 +16,25 @@ print("---------------------------------------------")
 print("what is the most important task?")
 priority = input("tell me:")
 print("Do it fast as you can !"+priority)
+"""
+
+#day 2
+print("python learning day 2")
+
+tasks = []
+
+print("Enter the tasks you have to do today: (press enter to stop)\n")
+
+while True:
+    task = input("Task: ")
+    
+    if task == "":
+        break 
+    tasks.append(task)
+
+print ("\n Your To-Do list for today is:\n")
+
+for i,t in enumerate(tasks):
+    print(f"{i+1}. {t}")
+print ("---------------------------------------------")
+print ("\n The number of tasks you have today is:" + str(len(tasks)))
